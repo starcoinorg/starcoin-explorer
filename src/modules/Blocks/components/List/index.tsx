b84f07f1bd2bce474a845a74b4cbd7b7235259f9
wk3368 <wk3368@163.com>
2020-12-27 22:25:24 +0800

    block list sort by height desc

--- src/modules/Blocks/components/List/index.tsx
+++ src/modules/Blocks/components/List/index.tsx
@@ -1,11 +1,10 @@
 import React, { PureComponent } from 'react';
-import BaseRouteLink from "@/common/BaseRouteLink";
 import IconButton from '@material-ui/core/IconButton';
 import ArrowBackIos from '@material-ui/icons/ArrowBackIos';
 import ArrowForwardIos from '@material-ui/icons/ArrowForwardIos';
 // import withLoading from '@/common/LoadingMasker/withLoading';
-// import BaseRouteLink from "@/common/BaseRouteLink";
-import { withStyles, Theme, createStyles, makeStyles } from '@material-ui/core/styles';
+import BaseRouteLink from "@/common/BaseRouteLink";
+import { withStyles, Theme, createStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -52,19 +51,6 @@ const useStyles = (theme: Theme) => ({
     minWidth: 700,
   },
 });
-
-function createData(name: string, calories: number, fat: number, carbs: number, protein: number) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
 
 class Index extends PureComponent<IndexProps, IndexState> {
   static defaultProps = {
@@ -125,7 +111,7 @@ class Index extends PureComponent<IndexProps, IndexState> {
               </TableHead>
               <TableBody>
                 {
-                  hits.map((row: any) => {
+                  hits.sort((a: any, b: any) => b._source.header.number - a._source.header.number).map((row: any) => {
                     const header = row._source.header;
                     const blockUrl = `/blocks/detail/${header.block_hash}`;
                     const authorUrl = `/author/${header.author}`;
