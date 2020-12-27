7d662e945184eb18580e56103202f3027ddc7096
wk3368 <wk3368@163.com>
2020-12-28 00:04:39 +0800

    confirm transaction list display columns

--- src/modules/Transactions/components/List/index.tsx
+++ src/modules/Transactions/components/List/index.tsx
@@ -1,12 +1,20 @@
 import React, { PureComponent } from 'react';
-import BaseRouteLink from "@/common/BaseRouteLink";
 import IconButton from '@material-ui/core/IconButton';
 import ArrowBackIos from '@material-ui/icons/ArrowBackIos';
 import ArrowForwardIos from '@material-ui/icons/ArrowForwardIos';
 // import withLoading from '@/common/LoadingMasker/withLoading';
-// import BaseRouteLink from "@/common/BaseRouteLink";
+import BaseRouteLink from "@/common/BaseRouteLink";
+import { withStyles, Theme, createStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
 
 interface IndexProps {
+  classes: any;
   transactionList: any;
   getTransactionList: (data: any, callback?: any) => any;
 }
@@ -15,6 +23,33 @@ interface IndexState {
   currentPage: number
 }
 
+const StyledTableCell = withStyles((theme: Theme) =>
+  createStyles({
+    head: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    body: {
+      fontSize: 14,
+    },
+  }),
+)(TableCell);
+
+const StyledTableRow = withStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover,
+      },
+    },
+  }),
+)(TableRow);
+
+const useStyles = (theme: Theme) => ({
+  table: {
+    minWidth: 700,
+  },
+});
 class Index extends PureComponent<IndexProps, IndexState> {
   static defaultProps = {
     transactionList: null,
@@ -53,6 +88,7 @@ class Index extends PureComponent<IndexProps, IndexState> {
     if (!transactionList) {
       return null;
     }
+    const { classes } = this.props;
     const hits = transactionList.hits.hits;
     const size = 20;
     const from = (this.state.currentPage -1) * size + 1;
@@ -61,6 +97,25 @@ class Index extends PureComponent<IndexProps, IndexState> {
       <React.Fragment>
         <div>
           Transactions List
+          <TableContainer component={Paper}>
+            <Table className={classes.table} aria-label="customized table">
+              <TableBody>
+                {
+                  hits.map((row: any) => {
+                    const transaction_hash = row._source.transaction_hash;
+                    const transactionUrl = `/transactions/detail/${transaction_hash}`;
+                    return(
+                      <StyledTableRow key={transaction_hash}>
+                        <StyledTableCell component="th" scope="row">
+                          <BaseRouteLink to={transactionUrl}>{transaction_hash}</BaseRouteLink>
+                        </StyledTableCell>
+                      </StyledTableRow>
+                    )
+                  })
+                }
+              </TableBody>
+            </Table>
+          </TableContainer>
           <p>
             {from} - {to}
             <IconButton aria-label="prev" onClick={() => this.pagination('prev')} disabled={this.state.currentPage === 1} >
@@ -70,29 +125,10 @@ class Index extends PureComponent<IndexProps, IndexState> {
               <ArrowForwardIos />
             </IconButton>
           </p>
-          <ul>
-          {
-            hits.map((item: any) => {
-              const url = `/transactions/detail/${item._id}`;
-              return(
-                <li key={item._id}>
-                  <BaseRouteLink to={url}>{item._id}</BaseRouteLink>
-                </li>
-              )
-            })
-          }
-          </ul>
-          <hr />
-          <h3>xhr response:</h3>
-          <div style={{ width: '80%', height: '400px', overflow: 'auto' }}>
-            <pre>
-              {JSON.stringify(transactionList,null, 2)}
-            </pre>
-          </div>
         </div>
       </React.Fragment>
     );
   }
 }
 
-export default Index;
+export default withStyles(useStyles)(Index);
