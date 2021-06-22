a56a76d9fa28135ed55826fd45b62f4e91b0783b
Bingo Du <tarvos21@gmail.com>
2021-06-22 10:23:54 +0800

    Add View component to show resources based on ResourceViewTable

---
+++ src/common/View/ResourceView/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+// import EventViewTable from '@/common/View/EventViewTable';
+import { createStyles, withStyles, Theme } from '@material-ui/core/styles';
+import ResourceViewTable from '@/common/View/ResourceViewTable';
+
+const useStyles = (theme: Theme) => createStyles({
+  [theme.breakpoints.down('sm')]: {
+    firstRow: {
+    },
+  },
+  [theme.breakpoints.up('sm')]: {
+    firstRow: {
+    },
+  },
+  firstRow: {
+  },
+});
+
+interface ExternalProps {
+  resources: any,
+  className?: string,
+}
+
+class Index extends React.PureComponent<ExternalProps> {
+  render() {
+    const { resources } = this.props;
+    const resourcesTable: any[] = [];
+    if (resources) {
+      for (let i = 0; i < resources.length; i++) {
+        const columns: any[] = [];
+        columns.push(['Key', resources[i][0]]);
+        columns.push(['Value', resources[i][1]]);
+        resourcesTable.push(<ResourceViewTable key={i} columns={columns} />);
+      }
+    }
+    return resourcesTable;
+  }
+}
+
+export default withStyles(useStyles)(Index);
