26730493405be0c29bbfcd96e87181d8a5bc6e26
Bingo Du <tarvos21@gmail.com>
2021-06-24 17:53:33 +0800

    Add Detail component to display info of a specific uncle block

---
+++ src/modules/UncleBlocks/components/Detail/adapter.ts
@@ -0,0 +1,32 @@
+import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
+import createLoadingSelector  from '@/rootStore/loading/selector';
+import store from '@/Blocks/store';
+import * as types from '@/Blocks/store/constants';
+import * as typesTransactions from '@/Transactions/store/constants';
+import storeTransactions from '@/Transactions/store';
+import Index from './index';
+
+const { selector: currentSelector, actions } = store;
+const { selector: currentSelectorTransactions, actions: actionsTransactions } = storeTransactions;
+
+const loadingSelector = createLoadingSelector([types.GET_UNCLE_BLOCK, types.GET_UNCLE_BLOCK_BY_HEIGHT, typesTransactions.GET_BLOCK_TRANSACTIONS, typesTransactions.GET_BLOCK_TRANSACTIONS_BY_HEIGHT]);
+
+const selector = createSelector(
+  currentSelector,
+  currentSelectorTransactions,
+  loadingSelector,
+  (current, transactions, loading) => ({
+    block: current.block,
+    uncleBlock: current.uncleBlock,
+    blockTransactions: transactions.blockTransactions,
+    loading
+  })
+);
+
+export default connect(selector, {
+  getUncleBlock: actions.getUncleBlock,
+  getUncleBlockByHeight: actions.getUncleBlockByHeight,
+  getBlockTransactions: actionsTransactions.getBlockTransactions,
+  getBlockTransactionsByHeight: actionsTransactions.getBlockTransactionsByHeight
+})(Index) as any;
---
+++ src/modules/UncleBlocks/components/Detail/index.tsx
@@ -0,0 +1,121 @@
+import React, { PureComponent } from 'react';
+import { withTranslation } from 'react-i18next';
+import { createStyles, withStyles } from '@material-ui/core/styles';
+import Loading from '@/common/Loading';
+import PageView from '@/common/View/PageView';
+import CommonLink from '@/common/Link';
+import formatNumber from '@/utils/formatNumber';
+
+const useStyles = () => createStyles({
+  table: {
+    width: '100%',
+    display: 'block',
+  },
+  shrinkMaxCol: {
+    flex: '1 100 auto',
+    minWidth: 60,
+  },
+  shrinkCol: {
+    flex: '1 10 auto',
+  },
+});
+
+interface IndexProps {
+  classes: any;
+  t: any;
+  match: any;
+  uncleBlock: any;
+  getUncleBlock: (data: any, callback?: any) => any;
+  getUncleBlockByHeight: (data: any, callback?: any) => any;
+}
+
+interface IndexState {
+  epochData: any,
+  hash?: string,
+  height?: string,
+}
+
+class Index extends PureComponent<IndexProps, IndexState> {
+  // eslint-disable-next-line react/static-property-placement
+  static defaultProps = {
+    match: {},
+    uncleBlock: null,
+    getUncleBlock: () => { },
+    getUncleBlockByHeight: () => { },
+  };
+
+  constructor(props: IndexProps) {
+    super(props);
+    this.state = {
+      epochData: undefined,
+      hash: props.match.params.hash,
+      height: props.match.params.height
+    };
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  static getDerivedStateFromProps(nextProps: any, prevState: any) {
+    // switch hash only in current page, won't switch height
+    // so only need to empty height while switch to /hash/xxx from height/xxx
+    if (nextProps.match.params.hash !== prevState.hash) {
+      return { ...prevState, hash: nextProps.match.params.hash, height: '' };
+    }
+    return null;
+  }
+
+  componentDidUpdate(prevProps: any, prevState: any) {
+    if (prevProps.match.params.hash !== this.state.hash && prevState.hash !== this.state.hash) {
+      this.fetchData();
+    }
+  }
+
+  fetchData() {
+    const hash = this.state.hash;
+    const height = this.state.height;
+    if (hash) {
+      this.props.getUncleBlock({ hash });
+    }
+    if (height) {
+      this.props.getUncleBlockByHeight({ height });
+    }
+  }
+
+  render() {
+    const { uncleBlock, match, t } = this.props;
+    const network = match.params.network;
+    const isInitialLoad = !uncleBlock;
+    if (isInitialLoad) {
+      return <Loading />;
+    }
+    if (!uncleBlock.header) {
+      return null;
+    }
+    const header = uncleBlock.header;
+    const columns = [
+      [t('common.Hash'), header.block_hash],
+      [t('uncleblock.Height'), formatNumber(header.number)],
+      [t('common.Time'), new Date(parseInt(header.timestamp, 10)).toLocaleString()],
+      [t('uncleblock.Author'), <CommonLink key={header.author} path={`/${network}/address/${header.author}`} title={header.author} />],
+      [t('uncleblock.BlockAccumulatorRoot'), header.block_accumulator_root],
+      [t('uncleblock.Difficulty'), formatNumber(header.difficulty_number)],
+      [t('common.GasUsed'), formatNumber(header.gas_used)],
+      [t('common.Nonce'), formatNumber(header.nonce)],
+      [t('uncleblock.ParentHash'), <CommonLink key={header.parent_hash} path={`/${network}/blocks/detail/${header.parent_hash}`} title={header.parent_hash} />],
+    ];
+
+    return (
+      <PageView
+        id={header.block_hash}
+        title={t('uncleblock.header')}
+        name={t('uncleblock.header')}
+        pluralName={t('uncleblock.header')}
+        bodyColumns={columns}
+      />
+    );
+  }
+}
+
+export default withStyles(useStyles)(withTranslation()(Index));
