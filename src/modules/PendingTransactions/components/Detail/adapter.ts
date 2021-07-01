dd8bcda08a1f96f930a064ee74f76c58cc91e435
Bingo Du <tarvos21@gmail.com>
2021-07-01 12:16:53 +0800

    Add Pending Transaction List and Detail pages

---
+++ src/modules/PendingTransactions/components/Detail/adapter.ts
@@ -0,0 +1,23 @@
+import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
+import createLoadingSelector  from '@/rootStore/loading/selector';
+import store from '@/Transactions/store';
+import * as types from '@/Transactions/store/constants';
+import Index from './index';
+
+const { selector: currentSelector, actions } = store;
+
+const loadingSelector = createLoadingSelector([types.GET_PENDING_TRANSACTION]);
+
+const selector = createSelector(
+  currentSelector,
+  loadingSelector,
+  (current, loading) => ({
+    pendingTransaction: current.pendingTransaction,
+    loading
+  })
+);
+
+export default connect(selector, {
+  getPendingTransaction: actions.getPendingTransaction
+})(Index) as any;
