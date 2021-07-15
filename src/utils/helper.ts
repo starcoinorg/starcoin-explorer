af901e43b88cebe937ad46c80759da3e8fdc1bbf
钟泽方 <zefangzhong@gmail.com>
2021-07-15 17:04:15 +0800

    fix(getnetwork): networkInpath should match the proper networks (#71)
    Co-authored-by: zhongzefang001 <zhongzefang001@ke.com>

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -6,34 +6,37 @@ export function withBaseRoute(route: any = '') {
   if (typeof route === 'object' && 'pathname' in route) {
     return {
       ...route,
-      pathname: `${route.pathname}`
-    }
+      pathname: `${route.pathname}`,
+    };
   }
   return route;
 }
 
 export function getNetwork() {
-  const networkInPath = window.location.pathname.split('/')[1]
-  let network = networkInPath || localStorage.getItem('network');
+  const networks = process.env.REACT_APP_STARCOIN_NETWORKS || '';
+  const networkArr = networks.split(',');
+  const networkInPath = window.location.pathname.split('/')[1];
+  let network =
+    (networkArr.includes(networkInPath) && networkInPath) ||
+    localStorage.getItem('network');
   if (!network) {
-    const networks = process.env.REACT_APP_STARCOIN_NETWORKS || '';
-    localStorage.setItem('network', networks.split(',')[0]);
-    network = networks.split(',')[0]
+    localStorage.setItem('network', networkArr[0]);
+    network = networkArr[0];
   }
   return network;
 }
 
 export function isHex(num: string) {
-  return Boolean(num.match(/^0x[0-9a-f]+$/i))
+  return Boolean(num.match(/^0x[0-9a-f]+$/i));
 }
 
-export function formatBalance(num :string | number) {
+export function formatBalance(num: string | number) {
   const value = new BigNumber(num);
   const convertedValue = value.div(1000000000).toFormat();
   return convertedValue;
 }
 
-export function toObject(data: {}):string {
+export function toObject(data: {}): string {
   return JSON.stringify(data, (key, value) => {
     if (typeof value === 'bigint') {
       return value.toString();
@@ -42,7 +45,7 @@ export function toObject(data: {}):string {
   });
 }
 
-export function formatResources(data: any) : any {
+export function formatResources(data: any): any {
   let resourcesArray = [];
   let itemKey = '';
   let itemValue = '';
