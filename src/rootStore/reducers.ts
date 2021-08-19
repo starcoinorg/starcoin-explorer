b091e9aea7440e3dd4993abdd9c56d040a390090
Bingo Du <tarvos21@gmail.com>
2021-08-19 11:11:25 +0800

    Add token saga scope to rootStore

--- src/rootStore/reducers.ts
+++ src/rootStore/reducers.ts
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router'
 import blocks from '@/Blocks/store';
+import tokens from '@/Tokens/store';
 import transactions from '@/Transactions/store';
 import search from '@/Search/store';
 
 const reducers = {
   [transactions.SCOPENAME]: transactions.reducer,
   [blocks.SCOPENAME]: blocks.reducer,
+  [tokens.SCOPENAME]: tokens.reducer,
   [search.SCOPENAME]: search.reducer,
 };
 
--- src/rootStore/sagas.ts
+++ src/rootStore/sagas.ts
@@ -1,11 +1,13 @@
 import { all, fork } from 'redux-saga/effects';
 import blocks from '@/Blocks/store';
+import tokens from '@/Tokens/store';
 import transactions from '@/Transactions/store';
 import search from '@/Search/store';
 import routerSaga from './router/sagas';
 
 const sagas = [
   ...blocks.sagas,
+  ...tokens.sagas,
   ...transactions.sagas,
   ...search.sagas,
   ...routerSaga
