9a304f9829767d442cdca77c428f3ac74c9cd91d
Found Pan Tiger <renzholy@gmail.com>
2021-08-22 14:37:47 +0800

    Update apis.ts

--- src/modules/Blocks/store/apis.ts
+++ src/modules/Blocks/store/apis.ts
@@ -1,12 +1,27 @@
 // @ts-ignore
 import client from '@/utils/client';
+import seedClient from '@/utils/seed-client';
 import { getNetwork } from '@/utils/helper';
 
 const network = getNetwork();
 
-export const getBlock = (params: any) => client.get(`v2/block/${network}/hash/${params.hash}`);
-export const getUncleBlock = (params: any) => client.get(`v2/block/${network}/uncle/hash/${params.hash}`);
-export const getBlockByHeight = (params: any) => client.get(`v2/block/${network}/height/${params.height}`);
-export const getUncleBlockByHeight = (params: any) => client.get(`v2/block/${network}/uncle/height/${params.height}`);
-export const getBlockList = (params: any) => client.get(`v2/block/${params.network ? params.network : network}/page/${params.page}${params.total ? `?total=${params.total}` : ''}`);
-export const getUncleBlockList = (params: any) => client.get(`v2/block/${params.network ? params.network : network}/uncle/page/${params.page}`);
+export const getBlock = (params: any) =>
+  client.get(`v2/block/${network}/hash/${params.hash}`);
+export const getUncleBlock = (params: any) =>
+  client.get(`v2/block/${network}/uncle/hash/${params.hash}`);
+export const getBlockByHeight = (params: any) =>
+  client.get(`v2/block/${network}/height/${params.height}`);
+export const getUncleBlockByHeight = (params: any) =>
+  client.get(`v2/block/${network}/uncle/height/${params.height}`);
+export const getBlockList = (params: any) =>
+  client.get(
+    `v2/block/${params.network ? params.network : network}/page/${params.page}${
+      params.total ? `?total=${params.total}` : ''
+    }`,
+  );
+export const getUncleBlockList = (params: any) =>
+  client.get(
+    `v2/block/${params.network ? params.network : network}/uncle/page/${
+      params.page
+    }`,
+  );
