ffbd29ee3dc1d669d9298984debe74c119e06de7
Bingo Du <tarvos21@gmail.com>
2021-08-26 00:19:50 +0800

    Format supply and position numbers with precision on holder's list

--- src/modules/Tokens/components/HolderList/index.tsx
+++ src/modules/Tokens/components/HolderList/index.tsx
@@ -8,6 +8,7 @@ import Pagination from '@/common/View/Pagination';
 import Typography from '@material-ui/core/Typography';
 import CenteredView from '@/common/View/CenteredView';
 import { getNetwork } from '@/utils/helper';
+import { getTokenPrecision } from '@/utils/sdk';
 import HolderTable from '../HolderTable';
 
 const useStyles = () => createStyles({
@@ -35,6 +36,7 @@ interface Props extends ExternalProps, InternalProps { }
 
 interface IndexState {
   tokenTypeTag: string,
+  tokenPrecision: number,
   currentPage: number
 }
 
@@ -50,11 +52,13 @@ class Index extends PureComponent<Props, IndexState> {
     super(props);
     this.state = {
       currentPage: parseInt(props.match.params.page, 10) || 1,
+      tokenPrecision: 1,
       tokenTypeTag: props.match.params.token_type_tag || '',
     };
   }
 
   componentDidMount() {
+    this.fetchTokenPrecision(this.state.tokenTypeTag);
     this.fetchListPage(this.state.tokenTypeTag, this.state.currentPage);
   }
 
@@ -62,15 +66,29 @@ class Index extends PureComponent<Props, IndexState> {
     this.props.getTokenHolderList({ token_type_tag, page });
   };
 
+  fetchTokenPrecision = (token_type_tag: string) => {
+    if (token_type_tag) {
+      getTokenPrecision(token_type_tag).then(data => {
+        if (data) {
+          this.setState({ tokenPrecision: parseInt(data[0], 10) });
+        } else {
+          console.log('get precision failed')
+        }
+      });
+    }
+  };
+
   pagination = (type: string) => {
     const total = this.props.tokenHolderList && this.props.tokenHolderList.total || 0;
     if (type === 'prev' && this.state.currentPage > 1) {
       const page = this.state.currentPage - 1;
       const token_type_tag = this.state.tokenTypeTag;
+      this.fetchTokenPrecision(token_type_tag);
       this.props.getTokenHolderList({ token_type_tag, page, total }, () => { this.pagenationCallback(page); });
     } else if (type === 'next') {
       const page = this.state.currentPage + 1;
       const token_type_tag = this.state.tokenTypeTag;
+      this.fetchTokenPrecision(token_type_tag);
       this.props.getTokenHolderList({ token_type_tag, page, total }, () => { this.pagenationCallback(page); });
     }
   };
@@ -82,6 +100,7 @@ class Index extends PureComponent<Props, IndexState> {
 
   render() {
     const { tokenHolderList, classes, t, className, isLoadingMore } = this.props;
+    const precision = this.state.tokenPrecision ? this.state.tokenPrecision : 1;
     const isInitialLoad = !tokenHolderList;
     const hits = tokenHolderList && tokenHolderList.contents || [];
     const holders = hits.sort((a: any, b: any) => b.amount - a.amount);
@@ -90,6 +109,7 @@ class Index extends PureComponent<Props, IndexState> {
         tokenHolders={tokenHolderList}
         sizeVisibleAt="xs"
         authorVisibleAt="md"
+        tokenPrecision={precision}
       />
     ) : (
       <CenteredView>
