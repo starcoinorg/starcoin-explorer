2d2bc2b77dd152eca46321be30152983a81d7524
Bingo Du <tarvos21@gmail.com>
2021-08-26 00:19:07 +0800

    Fomart amount number with token precision on token info detail page

--- src/modules/Tokens/components/Detail/index.tsx
+++ src/modules/Tokens/components/Detail/index.tsx
@@ -19,6 +19,7 @@ import Typography from '@material-ui/core/Typography';
 import BaseRouteLink from '@/common/BaseRouteLink';
 // import PageViewTable from '@/common/View/PageViewTable';
 // import EventViewTable from '@/common/View/EventViewTable';
+import { getTokenPrecision } from '@/utils/sdk';
 
 const useStyles = () => createStyles({
   table: {
@@ -44,6 +45,7 @@ interface IndexProps {
 
 interface IndexState {
   token_type_tag?: string,
+  token_precision?: number,
 }
 
 class Index extends PureComponent<IndexProps, IndexState> {
@@ -84,6 +86,15 @@ class Index extends PureComponent<IndexProps, IndexState> {
   fetchData() {
     const token_type_tag = this.state.token_type_tag;
     if (token_type_tag) {
+      getTokenPrecision(token_type_tag).then(data => {
+        if (data) {
+          this.setState({ token_precision: parseInt(data[0], 10) });
+        } else {
+          // this.setState({ accountResources: 'noResource' });
+          console.log('get precision failed')
+        }
+      });
+
       this.props.getTokenInfo({ token_type_tag });
     }
   }
@@ -272,11 +283,12 @@ class Index extends PureComponent<IndexProps, IndexState> {
     ];
       */
 
+    const precision = this.state.token_precision ? this.state.token_precision : 1;
     const columns = [
       [t('token.address'), token[0].type_tag],
-      [t('token.totalsupply'), formatNumber(token[0].market_cap)],
+      [t('token.totalsupply'), formatNumber(token[0].market_cap / precision)],
       [t('token.holdercount'), formatNumber(token[0].addressHolder)],
-      [t('token.position'), formatNumber(token[0].volume)]
+      [t('token.position'), formatNumber(token[0].volume / precision)]
     ];
     return (
       <PageView
