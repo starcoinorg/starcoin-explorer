81211881cbab36616bc95bb08a76862665cc3ed6
git <git@github.com>
2021-10-22 20:39:29 +0800

    Add API calls to fetch address txn data

--- src/modules/Transactions/store/apis.ts
+++ src/modules/Transactions/store/apis.ts
@@ -10,5 +10,6 @@ export const getPendingTransaction = (params: any) => client.get(`v2/transaction
 export const getTransactionList = (params: any) => client.get(`v2/transaction/${params.network ? params.network : network}/page/${params.page}${params.page > 1 && params.after ? `?after=${params.after}` : ''}`);
 export const getPendingTransactionList = (params: any) => client.get(`v2/transaction/pending_txns/${params.network ? params.network : network}/page/${params.page}`);
 export const getAddressTransactions = (params: any) => client.get(`v2/transaction/${network}/byAddress/${params.hash}`);
+export const getAddressTransactionList = (params: any) => client.get(`v2/transaction/address/${network}/${params.hash}/page/${params.page}`);
 export const getBlockTransactions = (params: any) => client.get(`v2/transaction/${network}/byBlock/${params.hash}`);
 export const getBlockTransactionsByHeight = (params: any) => client.get(`v2/transaction/${network}/byBlockHeight/${params.height}`);
