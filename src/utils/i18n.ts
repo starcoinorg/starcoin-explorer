cc20f90895c84b5fe11b7b4ed43742478c6bc85d
wsc <386180127@qq.com>
2022-04-14 09:37:37 +0800

    fix bug  - language switching is not persistent

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -15,7 +15,6 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    lng: 'en',
     fallbackLng: 'en',
     debug: false,
     react: {
