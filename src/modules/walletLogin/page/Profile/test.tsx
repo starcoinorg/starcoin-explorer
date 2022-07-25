1beab275c0433d57cc50e1642fdbc2587378f233
xiaziheng <xiaziheng@aixuexi.com>
2022-07-25 08:41:22 +0800

    test login

--- src/modules/walletLogin/page/Profile/test.tsx
+++ src/modules/walletLogin/page/Profile/test.tsx
@@ -84,6 +84,39 @@ function App() {
                 // ...
             });
     }
+
+    const oauthSignIn = () => {
+        // Google's OAuth 2.0 endpoint for requesting an access token
+        var oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
+
+        // Create <form> element to submit parameters to OAuth 2.0 endpoint.
+        var form = document.createElement('form');
+        form.setAttribute('method', 'GET'); // Send as a GET request.
+        form.setAttribute('action', oauth2Endpoint);
+
+        // Parameters to pass to OAuth 2.0 endpoint.
+        var params: any = {
+            'client_id': '800652009519-dgcmhc64lmn188b3mfpli60dd7roifcn.apps.googleusercontent.com',
+            'redirect_uri': 'https://test.stcscan.io/user/test',
+            'response_type': 'token',
+            'scope': 'https://www.googleapis.com/auth/drive.metadata.readonly',
+            'include_granted_scopes': 'true',
+            'state': 'pass-through value'
+        };
+
+        // Add form parameters as hidden input values.
+        for (var p in params) {
+            var input = document.createElement('input');
+            input.setAttribute('type', 'hidden');
+            input.setAttribute('name', p);
+            input.setAttribute('value', params[p]);
+            form.appendChild(input);
+        }
+
+        // Add form to page and submit it to open the OAuth 2.0 endpoint.
+        document.body.appendChild(form);
+        form.submit();
+    }
     const googleRiredct = () => {
         signInWithRedirect(auth, provider);
     }
@@ -97,6 +130,9 @@ function App() {
         <div style={{
             color: '#fff'
         }} onClick={googleRiredct}>googleRiredct</div>
+        <div style={{
+            color: '#fff'
+        }} onClick={oauthSignIn}>oauthSignIn</div>
     </>
 }
 export default App;
